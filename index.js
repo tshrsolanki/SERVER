3f8f396581955e0ed036dabdc18834bf6380e74a
Tushar <tshrsolanki@gmail.com>
2023-01-19 00:23:53 +0530

    first

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const path = require("path");
 const cors = require("cors");
 app.use(cors());
 app.use("/", express.static(path.join(__dirname, "build")));
-app.listen(8000, () => {
-  console.log("running on port 8000", "||", "index.js", "line-", 8);
+app.listen(3000, () => {
+  console.log("running on port 3000", "||", "index.js", "line-", 8);
 });
